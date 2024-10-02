50016a23f632d2c7eed22fea68f2c851a8fd8615
Ranjith J Pillai <ranjithj.pillai@philips.com>
2024-10-03 01:42:36 +0530

    Reduced Complexity

--- monitor.cpp
+++ monitor.cpp
@@ -5,34 +5,45 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+void DisplayCriticalIdicator() {
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+    cout << std::endl;
+}
+
+bool IsTemperatureInFarenheitCritical(float temperature) {
+    if (temperature < Min_Normal_Temperature_F || temperature > Max_Normal_Temperature_F) {
+        cout << "Temperature is critical!\n";
+        return true;
+    }
+    return false;
+}
+
+bool IsPulseRateCritical(float pulseRate) {
+    if (pulseRate < Min_Normal_PulseRange || pulseRate > Max_Normal_PulseRange) {
+        cout << "Pulse Rate is out of range!\n";
+        return true;
+    }
+    return false;
+}
+
+bool IsOxygenSaturationCritical(float spo2) {
+    if (spo2 < Min_Normal_SPO2) {
+        cout << "Oxygen Saturation out of range!\n";
+        return true;
+    }
+    return false;
+}
+
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    if (IsTemperatureInFarenheitCritical(temperature) ||
+        IsPulseRateCritical(pulseRate) || IsOxygenSaturationCritical(spo2)) {
+        DisplayCriticalIdicator();
+        return 0;
     }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+    return 1;
 }
--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,7 +2,31 @@
 
 #include "gtest/gtest.h"
 
+TEST(Monitor, IndividualVital) {
+    ASSERT_TRUE(IsTemperatureInFarenheitCritical(93.0F));
+    ASSERT_TRUE(IsTemperatureInFarenheitCritical(105.0F));
+    ASSERT_FALSE(IsTemperatureInFarenheitCritical(95.0F));
+    ASSERT_FALSE(IsPulseRateCritical(70));
+    ASSERT_FALSE(IsPulseRateCritical(100));
+    ASSERT_TRUE(IsPulseRateCritical(110));
+    ASSERT_FALSE(IsOxygenSaturationCritical(90));
+    ASSERT_TRUE(IsOxygenSaturationCritical(88));
+    ASSERT_FALSE(IsOxygenSaturationCritical(97));
+}
+
+TEST(Monitor, IndividualVitalForPrecision) {
+    ASSERT_FALSE(IsTemperatureInFarenheitCritical(101.99999));
+    ASSERT_TRUE(IsPulseRateCritical(59.99999));
+    ASSERT_FALSE(IsPulseRateCritical(59.9999999));
+    ASSERT_TRUE(IsPulseRateCritical(100.00001));
+    ASSERT_FALSE(IsPulseRateCritical(100.0000001));
+    ASSERT_TRUE(IsOxygenSaturationCritical(89.9999));
+    ASSERT_FALSE(IsOxygenSaturationCritical(89.999999));
+}
+
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
+  ASSERT_FALSE(vitalsOk(103, 99, 70));
   ASSERT_FALSE(vitalsOk(99, 102, 70));
+  ASSERT_FALSE(vitalsOk(99, 70, 89));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
